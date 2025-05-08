6f044b55ef1e03e1c09bcf7cd099afa22bf7acbd
Brock M. Mowry <brock.m.mowry@gmail.com>
2025-05-08 12:35:48 -0400

    ...

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const mobileNavToggled = ref(false);
+
+function toggleMobileNav() {
+  mobileNavToggled.value = !mobileNavToggled.value;
+}
+</script>
 
 <template>
   <header class="header">
@@ -14,19 +22,44 @@
             <router-link :to="{ path: '/', hash: '#herstory-section' }">"Herstory"</router-link>
             <router-link :to="{ path: '/', hash: '#sponsors-section' }">Sponsors</router-link>
             <router-link :to="{ path: '/', hash: '#contact-section' }">Contact</router-link>
-            <a aria-current="page" href="https://www.wsbn.tv/channel-29-slow-pitch-ball/">Watch Live</a>
+            <a target="_blank" href="https://www.wsbn.tv/channel-29-slow-pitch-ball/">Watch Live</a>
           </nav>
-          <router-link :to="{ name: 'SponsorView' }" class="btn btn-outline-primary-light d-none d-md-block ms-3"> SPONSOR </router-link>
-          <button class="mobile-nav-toggler d-flex d-lg-none">
+          <router-link :to="{ name: 'SponsorView' }" class="btn btn-outline-primary-light ms-0 ms-lg-3"> SPONSOR </router-link>
+          <button @click="toggleMobileNav" class="mobile-nav-toggler d-flex d-lg-none">
             <i class="bi bi-list"></i>
           </button>
         </div>
+      </div>
+    </div>
+    <div class="mobile-nav" :class="{ active: mobileNavToggled }">
+      <div class="mobile-nav-header">
+        <div class="d-flex align-items-center justify-content-between w-100">
+          <router-link @click="toggleMobileNav" :to="{ name: 'HomeView' }" class="mobile-nav-logo">
+            <img src="../assets/images/dits-logo.png" alt="Dirt in the Skirt (2025) Logo" />
+          </router-link>
+          <button @click="toggleMobileNav" class="mobile-nav-toggler d-flex d-lg-none">
+            <i class="bi bi-list"></i>
+          </button>
+        </div>
+      </div>
+      <nav class="mobile-nav-list w-100">
+        <router-link @click="toggleMobileNav" :to="{ name: 'HomeView' }">Home</router-link>
+        <router-link @click="toggleMobileNav" :to="{ path: '/', hash: '#about-section' }">About</router-link>
+        <router-link @click="toggleMobileNav" :to="{ path: '/', hash: '#herstory-section' }">"Herstory"</router-link>
+        <router-link @click="toggleMobileNav" :to="{ path: '/', hash: '#sponsors-section' }">Sponsors</router-link>
+        <router-link @click="toggleMobileNav" :to="{ path: '/', hash: '#contact-section' }">Contact</router-link>
+        <a @click="toggleMobileNav" target="_blank" href="https://www.wsbn.tv/channel-29-slow-pitch-ball/">Watch Live</a>
+      </nav>
+      <div class="mobile-nav-footer">
+        <p class="mb-0">&copy; Silver Compass Consulting, 2025</p>
       </div>
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/styles/colors" as c;
+
 .header {
   position: absolute;
   top: 0;
@@ -61,30 +94,112 @@
   }
 }
 
-.mobile-nav-toggler {
-  width: 45px;
-  height: 45px;
+.mobile-nav {
+  position: fixed;
+  top: 0;
+  right: -100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: none;
-  border: none;
-  outline: none;
-  border-radius: 0.5rem;
-  transition: background-color 0.3s ease;
+  align-items: flex-start;
+  justify-content: space-between;
+  width: 100%;
+  height: 100vh;
+  padding: 2rem;
+  background-color: c.$headings;
+  transition:
+    right 0.3s ease,
+    width 0.3s ease;
 
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.1);
+  &.active {
+    right: 0;
   }
 
-  &:active {
-    background-color: rgba(255, 255, 255, 0.2);
+  @media (min-width: 992px) {
+    &.active {
+      right: -100%;
+    }
   }
 
-  i {
-    color: #fff;
-    font-size: 30px;
+  @media (max-width: 992px) {
+    width: 60%;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  &-logo img {
+    width: auto;
+    height: 70px;
+  }
+
+  &-header,
+  &-list,
+  &-footer {
+    width: 100%;
+  }
+
+  &-header {
+    border-bottom: 1px solid rgba(white, 0.1);
+    padding-bottom: 1rem;
+  }
+
+  &-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    row-gap: 2rem;
+
+    a:not(.btn) {
+      color: rgba(white, 0.7);
+      font-weight: 500;
+      font-size: 2rem;
+      text-decoration: none;
+      text-transform: uppercase;
+      transition: color 0.3s ease;
+
+      &:hover {
+        color: white;
+      }
+    }
+  }
+
+  &-footer {
+    border-top: 1px solid rgba(white, 0.1);
+    padding-top: 1rem;
+    text-align: center;
+
+    p {
+      color: rgba(white, 0.7);
+    }
+  }
+
+  &-toggler {
+    width: 45px;
+    height: 45px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: none;
+    border: none;
+    outline: none;
+    border-radius: 0.5rem;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.1);
+    }
+
+    &:active {
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    i {
+      color: #fff;
+      font-size: 30px;
+    }
   }
 }
 </style>
--- src/views/SponsorView.vue
+++ src/views/SponsorView.vue
@@ -3,6 +3,23 @@ import HeroSectionComponent from "../components/HeroSectionComponent.vue";
 import SpacerComponent from "../components/SpacerComponent.vue";
 import AboutSponsorSectionComponent from "../components/sponsor/AboutSponsorSectionComponent.vue";
 import SponsorsTiersSection from "../components/sponsor/SponsorsTiersSection.vue";
+import { onMounted, ref } from "vue";
+
+const sponsorshipDeck = ref("");
+const sponsorshipDeckLoading = ref(true);
+const sponsorshipDeckError = ref(false);
+
+onMounted(async () => {
+  const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/v1/sponsors/deck`);
+  if (response.ok) {
+    const responseJson = await response.json();
+    sponsorshipDeck.value = responseJson.data[0].pdfFile;
+  } else {
+    sponsorshipDeckError.value = true;
+  }
+
+  sponsorshipDeckLoading.value = false;
+});
 </script>
 
 <template>
@@ -14,14 +31,9 @@ import SponsorsTiersSection from "../components/sponsor/SponsorsTiersSection.vue
           Join us in making an impact while gaining premium exposure at
           <span class="text-primary-light">one of the most exciting women's sports events in the region.</span>
         </p>
-        <div class="d-flex align-items-center justify-content-center gap-2">
-          <a
-            href="https://silvercoco.ca/wp-content/uploads/2025/04/Copy-of-DITS-Sponsorship-Deck-2025.pdf"
-            class="btn btn-outline-primary-light mt-4"
-          >
-            View Sponsorship PDF
-          </a>
-        </div>
+        <div v-if="sponsorshipDeckLoading" class="spinner spinner-border text-white mt-4"></div>
+        <div v-else-if="sponsorshipDeckError" class="alert alert-danger mt-4">Error fetching DITS 2025 Sponsorship Deck. Please try again later.</div>
+        <a v-else :href="sponsorshipDeck" target="_blank" class="btn btn-outline-primary-light mt-4"> View Sponsorship PDF </a>
       </div>
     </div>
   </HeroSectionComponent>
